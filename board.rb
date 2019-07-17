f94e7cbeb119e21fd1a1da581c688900124b1750
Gustavo S Frehse <gusfrehse@gmail.com>
2019-07-17 23:00:53 +0000

    Board#put_ship is almost finished, the only left is to be able to change the head position

--- board.rb
+++ board.rb
@@ -1,7 +1,10 @@
 class Board
-  @@ships = { :A => 'A+++++',
-              :B => 'B++',
-              :C => 'C', }
+  @@ships = { :A => [ ['A', '+', '+'] ],
+              :B => [ ['B', '+', '~'],
+                      ['+', '+', '+'] ],
+              :C => [ ['C'],
+                      ['+'],
+                      ['+'] ] }
 
   def initialize( width, height )
     raise ArgumentError, 'width and height must be bigger than five and less \
@@ -9,12 +12,30 @@ class Board
     @width = width
     @height = height
     @board = ( Array.new( height ) { Array.new( width ) } )
-    @board = @board.collect { |l| l.collect { 0 } }
+    @board = @board.collect { |l| l.collect { '~' } }
   end
 
-  def put_ship( code, x, y )
-    raise ArgumentError, 'position (x and y) must be inside the board' \
-      unless x < @width - 1 && x > -1 && y < @height - 1 && y > -1
+  def put_ship( code, hx, hy )
+    # TODO : the ship's head is expected to be in the top left corner in the
+    # design, we don't want that in the final result
+    hdy = 0
+    hdx = 0
+    @@ships[code].each_with_index do |line, dy|
+      line.each_with_index do |value, dx|
+        by = dy - hdy + hy
+        bx = dx - hdx + hx
+        unless value == '~'
+          raise ArgumentError, 'desired position is not inside the board' \
+            if by > @height - 1 || by < 0 || bx > @width - 1 || bx < 0
+         
+          raise ArgumentError, "there's a ship in the desired position" \
+            if @board[by][bx] != '~'
+          @board[by][bx] = value
+        end
+      end
+    end
+#    raise ArgumentError, 'position (x and y) must be inside the board' \
+#      unless x < @width - 1 && x > -1 && y < @height - 1 && y > -1
   end
 
   def to_s
@@ -29,6 +50,7 @@ class Board
   end
 end
 b = Board.new( 10, 10 )
-p b
 b.put_ship( :A, 6, 5 )
+b.put_ship( :B, 4, 3 )
+b.put_ship( :C, 0, 0 )
 puts b.to_s
