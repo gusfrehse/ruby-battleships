e546f9b130c1d92248e011a6b02752f769d7155b
Gustavo S Frehse <gusfrehse@gmail.com>
2019-07-17 21:40:02 +0000

    Made the code fit in 80 columns, maybe I reverted some old things by accident

--- board.rb
+++ board.rb
@@ -4,38 +4,22 @@ class Board
               :C => 'C', }
 
   def initialize( width, height )
-    raise ArgumentError, 'width and height must be bigger than five and less than eleven.' unless \
-      width > 5 && width < 11 && height > 5 && height < 11
+    raise ArgumentError, 'width and height must be bigger than five and less \
+      than eleven.' unless width > 5 && width < 11 && height > 5 && height < 11
     @width = width
     @height = height
-    @board = ( Array.new( height ) { Array.new( width ) } ).collect { |l| l.collect { '~' } }
+    @board = ( Array.new( height ) { Array.new( width ) } )
+    @board = @board.collect { |l| l.collect { 0 } }
   end
 
   def put_ship( code, x, y )
-    raise ArgumentError, 'position of the \'head\' (x and y) must be inside the board' unless \
-      x < @width - 1 && x > -1 && y < @height - 1 && y > -1
-    # Check if ship fits in the horizontal direction
-    raise ArgumentError, 'the ship does not fit in the board at the specified position' if \
-      @@ships[code].length >= @width - x 
-    # It fits in the horizontal
-    # By now there only ships in the horizontal so i am not checking for the vertical
-    # TODO : need a way to check if it will fit with another ship already
-    # Put the ship in the desired position
-    @@ships[code].each_char.with_index do |e, index| 
-      if @board[y][x + index] != '~'
-        raise ArgumentError, 'the ship does not fit because there is already a ship in the desired place'
-      else
-        @board[y][x + index] = e
-      end
-    end
+    raise ArgumentError, 'position (x and y) must be inside the board' \
+      unless x < @width - 1 && x > -1 && y < @height - 1 && y > -1
   end
 
   def to_s
-    ret = " "
-    (0...@width).each { |n| ret << " " + n.to_s + " " }
-    ret << "\n"
+    ret = ""
     @board.each_with_index do |line, y|
-      ret << y.to_s
       line.each_with_index do |value, x|
         ret << "|#{ value }|"
       end
@@ -45,6 +29,6 @@ class Board
   end
 end
 b = Board.new( 10, 10 )
-b.put_ship( :B, 3, 5 )
-b.put_ship( :C, 4, 6 )
+p b
+b.put_ship( :A, 6, 5 )
 puts b.to_s
--- main.rb
+++ main.rb
@@ -7,17 +7,23 @@ class Application
   def select_phase
     puts "Player 01 look to the pc, Player 02 look away"
     sleep 5
-    puts "Player 01, I just wanted to say you're so much more beatiful than Player 02.\nAnyway, here is your board"
+    puts "Player 01, I just wanted to say you're so much more beatiful than \
+      Player 02.\nAnyway, here is your board"
     print @@board01
-    puts "and here is your ships. Now, you need to understand something rather difficult.\nFirst, each ship looks like this:\n\
-A+++++\n\
-Each ship has a letter. This letter we will call CODE. The ship's CODE is what you can think as the name of the ship. \
-So when I ask for a ship you will NOT write the actual ship, but WILL write it's CODE. The CODE is also the 'head' of the ship (in this \
-case it is the letter 'A'). Do you understand? If not, too bad because I am a pre-written message.\n\
-Also, it is important for you to know what is position and what is the position of the ships. Position, at least in this \
-game is a combination of two numbers, a X (horizontal) and an Y (vertical) number. The position of the ship is based on \
-the position of the ship's CODE, which is the letter, which is the 'head' of the ship and which is the only character in \
-the ship different from '+'."
+    puts "and here is your ships. Now, you need to understand something \
+      rather difficult.\nFirst, each ship looks like this:\n\
+      A+++++\n\
+      Each ship has a letter. This letter we will call CODE. The ship's CODE \
+      is what you can think as the name of the ship. So when I ask for a ship \
+      you will NOT write the actual ship, but WILL write it's CODE. The CODE \
+      is also the 'head' of the ship (in this case it is the letter 'A'). Do \ 
+      you understand? If not, too bad because I am a pre-written message.\n\
+      Also, it is important for you to know what is position and what is the \
+      position of the ships. Position, at least in this game is a combination \
+      of two numbers, a X (horizontal) and an Y (vertical) number. The \ 
+      position of the ship is based on the position of the ship's CODE, which \
+      is the letter, which is the 'head' of the ship and which is the only \
+      character in the ship different from '+'."
   end
 
   def run( width, height )
